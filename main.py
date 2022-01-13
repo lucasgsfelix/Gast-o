251250cb20b608292fc9485d3017a4230742baba
lucasgsfelix <lucasgsfelix@gmail.com>
2022-01-13 11:18:37 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+
+
+
+
+
+if __name__ == '__main__':
+
+	# I need a login
+
+	# I need to take as input the salary
+
+	# If the salary is brute we need to measure how much is the liquid
+
+	# I need to automatically acess the sheet
+
+	# I need to consume for each user the budgets for their categories
+
+	# I need to automatically measure if any category is exploding any budget
+
+	# Show the graphs
+
+ 
+
